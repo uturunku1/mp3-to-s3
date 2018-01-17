684c35c00743a888f408fe961a2cfdfe6b930cff
stella huayhuaca <luces.huayhuaca@gmail.com>
2018-01-16 21:08:50 -0800

    modify async module, add README  and change audiobitrate to 256

---
+++ README.md
@@ -0,0 +1,43 @@
+# MP3-To-S3
+
+## Description
+This Lambda function takes a youtube video Id (from a custom Event JSON object) and converts the video into an MP3 file using the Node.JS library YoutubeMP3Downloader. Then it saves it to a S3 bucket and returns the S3 URL link. These node modules and FFMPEG binaries are statically built for Amazon Linux System.
+
+## Lambda Settings by running bash script
+
+- Clone this repository
+- in file `deploy.sh` make sure to change value of variable ROLE to your correct arn role and your preferred S3 region.
+- in file `input.txt` adjust youtube video Id and bucket name. Warning: do not edit `input.txt` outside of a code editor.
+- In terminal cd into the root directory of this folder and enter:
+- $chmod +x deploy.sh
+- $./deploy.sh
+
+## Lambda Settings Manually
+
+- Clone repository
+- zip file repository from terminal: zip -r ../index.zip *
+- upload zip file to S3 bucket (because the size is more than 10MB)
+- Create Function in Lambda console, using the following settings:
+- Upload a file from Amazon S3: Code entry type
+- Runtime: `Node.js 6.10`
+- Handler: `index.handler`
+- Timeout: 33 sec
+- Configure test event: with event structure given bellow
+
+### Basic Settings
+
+Set the memory to `1536 MB`(XD) AND increase the timeout to `23 sec`.
+
+### Execution Role
+
+To use S3, you will need to create a execution role with the correct permission.
+
+### Triggers and Data
+The trigger event will be a JSON object. If the videoUrl property is set to null, the program will look for a MP3 file in a selected S3 bucket.
+
+Event structure:
+
+{
+  "videoUrl": "lRs72x7Lgtc",
+  "bucket": "democracy-live"
+}
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /*jshint esversion: 6 */
 'use strict';
 const aws = require('aws-sdk');
-// const execute = require('lambduh-execute');
 const fs = require('fs');
 var Downloader = require("./downloader");
 const s3 = new aws.S3();
@@ -11,29 +10,31 @@ const mp3Filename=`${(Date.now()).toString()}`+".mp3";
 exports.handler=(event, context, callback)=>{
     console.log('mp3Filename is: '+ mp3Filename);
     var videoUrl= event.videoUrl;
+    var audio= event.audio;
     console.log("event videoUrl is: "+ videoUrl);
-    if (videoUrl) {
-        console.log('inside of if statement and...');
+    if (videoUrl && videoUrl!=null) {
         var i= 0;
         dl.getMP3({videoId: videoUrl, name: mp3Filename},function(error,data){
-            console.log("videoId is here!  "+videoUrl);
             i++;
             if(error) throw error;
             else{
-                console.log("Song "+ i+" was downloaded: "+ data.file);
-                var s3Path= saveToS3(data.file, event.bucket);
-                // console.log(s3Path);
+                console.log("Audio "+ i+" was downloaded: "+ data.file);
+                saveToS3(data.file, event.bucket);
             }
         });
+    }else {
+        console.log('we have an audio already normalized for Alexa');
+        //code to normalize audio for Alexa requirements
     }
-    callback(null,'success uploading Alexa mp3 audio to S3');
+    callback(null,'Uploading Alexa mp3 audio to S3...');
 };//end of exports.
 
 function saveToS3(mp3TempFile, bucket) {
     fs.readFile(mp3TempFile, function (err, data) {
       if (err) throw err;
       var param = {Bucket: bucket, Key: mp3Filename, Body: data};
-      var s3Path = 'http://'+bucket+'.s3.amazonaws.com/'+mp3Filename;
+      // var s3Path = 'http://'+bucket+'.s3.amazonaws.com/'+mp3Filename;
+      var s3Path = 'https://s3-us-west-2.amazonaws.com/'+bucket+'/'+mp3Filename;
       s3.upload(param, function(err, data) {
 
           // Whether there is an error or not, delete the temp file
